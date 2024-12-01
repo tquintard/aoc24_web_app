0e3b3822e3576820e4bacb87b8015e3daefa24ea
tquintard <tquintard@gmail.com>
2024-12-01 17:02:07 +0000

    Test1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Bienvenue sur ma page Streamlit!")
-st.write("Ceci est un exemple de déploiement avec GitHub.")
+st.write("Ceci est un exemple de déploiement avec GitHub. Test nouvel phrase")
