7fab2464944c09eb67591bafff6c9a2374d2736f
tquintard <tquintard@gmail.com>
2024-12-01 18:16:52 +0000

    Update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("Bienvenue sur ma page Streamlit!")
-st.write("Ceci est un exemple de déploiement avec GitHub. Test nouvelle phrase")
+st.title("Welcome to my Advent of Code 2024 web app")
+st.write("This web app aims to share my python code I used for each AoC day's puzzle")
+st.write("Ultimately, you can drop your inputs and run my code on it to check wether your code is correct or not ;)")
