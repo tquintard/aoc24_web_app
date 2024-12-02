5e63dab9062144a87592eeccd5e4611577a413be
tquintard <tquintard@gmail.com>
2024-12-02 00:09:43 +0000

    Update

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ else:
                 # else:
                 #     elapsed_time = f"{int(elapsed_time) + 1} ms"
                 #     solution = len(input_data.splitlines())
-                st.success(st.markdown(f"""
+                st.markdown(st.success(f"""
                             **Your solution for Day {day} are:**
                             - **Part 1:** {sol[0]}
                             - **Part 2:** {sol[1]}
