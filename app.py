423b7787f3f8b29382ee3a670b577aebc5e3a899
tquintard <tquintard@gmail.com>
2024-12-02 01:15:32 +0000

    update

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ else:
     day = option.split(":")[0].split()[1]
 
     # Create tabs for "Solver" and "Code"
-    tab1, tab2 = st.tabs(["Solver 🧩", "Code Viewer 📜"])
+    tab1, tab2 = st.tabs(["🧩Solver", "📜Code Viewer"])
 
     # Solver Tab
     with tab1:
